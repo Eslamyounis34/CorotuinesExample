0f4856f54fccc316a0f0a2a8b6ca9208a209649f
Eslamyounis34 <eslamyounis34@gmail.com>
2021-11-21 02:31:16 +0200

    Add Binding To Adapter

--- app/src/main/java/com/example/corotuinesexample/ui/adapters/NewsRecyclerView.kt
+++ app/src/main/java/com/example/corotuinesexample/ui/adapters/NewsRecyclerView.kt
@@ -1,12 +1,13 @@
 package com.example.corotuinesexample.ui.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.corotuinesexample.R
+import com.example.corotuinesexample.databinding.NewsrecycleritemrowBinding
 import com.squareup.picasso.Picasso
 import com.younis.newapp.model.Article
 import com.younis.newapp.model.OnArticleListner
@@ -14,30 +15,26 @@ import com.younis.newapp.model.OnArticleListner
 class NewsRecyclerView(private val articles: List<Article>) :
     RecyclerView.Adapter<NewsRecyclerView.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var articleImage: ImageView = itemView.findViewById(R.id.newsimage)
-        var articleTitle: TextView = itemView.findViewById(R.id.newstitle)
+    class ViewHolder(var binding: NewsrecycleritemrowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
     }
 
     private var onItemClickListener: OnArticleListner? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            NewsrecycleritemrowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         var v =
             LayoutInflater.from(parent.context).inflate(R.layout.newsrecycleritemrow, parent, false)
-        return NewsRecyclerView.ViewHolder(v)
+        return ViewHolder(binding)
     }
 
- //   val article = getItem(position)
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.apply {
-            holder.articleTitle.text = articles[position].title
-            Picasso.get().load(articles[position].urlToImage).into(holder.articleImage)
-
-            setOnClickListener {
-                onItemClickListener?.onclick(articles[position]!!)
-            }
+        holder.binding.newstitle.text = articles[position].title
+        Picasso.get().load(articles[position].urlToImage).into(holder.binding.newsimage)
+        holder.binding.newrelativelayout.setOnClickListener {
+            onItemClickListener?.onclick(articles[position]!!)
         }
     }
 
