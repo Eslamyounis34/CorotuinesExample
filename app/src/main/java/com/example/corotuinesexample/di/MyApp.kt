c0002add1208f1cd8a167029de0a2ac63ffa46aa
Eslam-Youness <eslam-youness@elarabygroup.com>
2022-12-06 15:56:01 +0200

    add name app to manifest

---
+++ app/src/main/java/com/example/corotuinesexample/di/MyApp.kt
@@ -0,0 +1,8 @@
+package com.younis.newapp.model.com.example.corotuinesexample.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
